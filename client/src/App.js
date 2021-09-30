73a186be5da7b53cf0b039cf739589daa7ec0d84
PhamDai <daicaopham1026@gmail.com>
2021-09-30 11:35:27 +0700

    4. Chat function

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import io from 'socket.io-client'
 import { useState } from 'react';
+import Chat from './Chat';
 const socket = io.connect('http://localhost:3001')
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
       <input type="text"  placeholder='John...' onChange={(e) => setUsername(e.target.value)}/>
       <input type="text" placeholder='Room ID...' onChange={(e) => setRoom(e.target.value)} />
       <button onClick={joinRoom}>Join A Room</button>
+      <Chat socket={socket} username={username} room={room}/>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -21,6 +21,9 @@ io.on('connection', socket => {
     socket.join(data)
     console.log(`User with ID: ${socket.id} joined room: ${data}`)
   })
+  socket.on('send_message', data => {
+    socket.to(data.room).emit('receive_message', data)
+  } )
   socket.on('disconnect', () => console.log('User disconnected', socket.id))
 })
 
